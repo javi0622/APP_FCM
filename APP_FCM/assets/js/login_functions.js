ad2ca6c25e9d7ac70fef7459da7099bb02050046
javi0622 <39597603+javi0622@users.noreply.github.com>
2018-06-01 11:53:21 -0600

    Add files via upload

---
+++ APP_FCM/assets/js/login_functions.js
@@ -0,0 +1,31 @@
+$(document).on("click","#boton-portal-1", function(){
+$('#modal-heading').html('GERENCIA DE LOTERIA');
+});
+
+$(document).on("click","#boton-portal-2", function(){
+$('#modal-heading').html('GERENCIA DE IMPRENTA');
+});
+
+$(document).on("click","#boton-portal-3", function(){
+$('#modal-heading').html('UNIDAD DE INFORMATICA');
+});
+
+$(document).on("click","#boton-portal-4", function(){
+$('#modal-heading').html('GERENCIA PLANIFICACION');
+});
+
+$(document).on("click","#boton-portal-5", function(){
+$('#modal-heading').html('AUDITORIA INTERNA');
+});
+
+$(document).on("click","#boton-portal-6", function(){
+$('#modal-heading').html('GERENCIA FINANCIERA');
+});
+
+$(document).on("click","#boton-portal-7", function(){
+$('#modal-heading').html('DASHBOARD');
+});
+
+$(document).on("click","#boton-portal-8", function(){
+$('#modal-heading').html('OTROS');
+});
